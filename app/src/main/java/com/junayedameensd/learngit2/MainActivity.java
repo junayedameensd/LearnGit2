e0c055c1169de9feb942ae059e3ef88bd9bb2e70
Junayed Ameen <junayedameen@gmail.com>
2017-10-19 11:02:12 +0600

    master commit 2

--- app/src/main/java/com/junayedameensd/learngit2/MainActivity.java
+++ app/src/main/java/com/junayedameensd/learngit2/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setTitle("Initial");
+        setTitle("master commit 2");
+
     }
 }
